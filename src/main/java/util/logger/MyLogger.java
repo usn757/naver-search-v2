7b6a150c8609bab5582d06fdac3524ce968fed92
usn757 <usn757@gmail.com>
2025-03-06 16:15:08 +0900

    buildtest

--- src/main/java/util/logger/MyLogger.java
+++ src/main/java/util/logger/MyLogger.java
@@ -12,17 +12,30 @@ public class MyLogger {
 
     public MyLogger(Class<?> clazz, AppConfig config) {
         logger = Logger.getLogger(clazz.getName());
+        String mode = config.getMode();
+        String logLevel = config.getLogLevel(); // LOG_LEVEL 환경변수 추가.
 
-        switch (config.getMode()) {
-            case "DEV":
-                this.level = Level.INFO;
-                break;
-            case "PROD":
-                this.level = Level.SEVERE;
-                break;
-            default:
-                this.level = Level.INFO;
+        // 명시적으로 LOG_LEVEL이 설정된 경우, 해당 레벨 사용
+        if (logLevel != null && !logLevel.isBlank()) {
+            try {
+                this.level = Level.parse(logLevel);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Invalid LOG_LEVEL: " + logLevel + ". Using INFO as default.");
+                this.level = Level.INFO; // 유효하지 않은 LOG_LEVEL이면 기본값 사용.
+            }
+        } else { // LOG_LEVEL이 설정되지 않은 경우, MODE에 따라 설정
+            switch (mode) {
+                case "DEV":
+                    this.level = Level.INFO;
+                    break;
+                case "PROD":
+                    this.level = Level.SEVERE;
+                    break;
+                default:
+                    this.level = Level.INFO;
+            }
         }
+
         logger.setLevel(this.level);
     }
 
